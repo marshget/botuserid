35eda15894ee1a6cd571bb1e4bfced85e7b12afa
marshget <marshbranded5@gmail.com>
2024-12-10 17:37:00 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,20 +2,25 @@ const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const fs = require('fs');
 require('dotenv').config();
 
+// Inisialisasi bot Discord
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+// Menyimpan commands ke koleksi
 client.commands = new Collection();
-const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
 
+// Memuat command files
+const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.name, command);
 }
 
+// Event: Bot siap
 client.once('ready', () => {
-    console.log(`${client.user.tag} is online!`);
+    console.log(`${client.user.tag} is online and ready!`);
 });
 
+// Event: Saat command di-trigger
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
 
@@ -27,8 +32,12 @@ client.on('interactionCreate', async interaction => {
         await command.execute(interaction);
     } catch (error) {
         console.error(error);
-        await interaction.reply({ content: 'There was an error executing this command!', ephemeral: true });
+        await interaction.reply({
+            content: 'There was an error while executing this command!',
+            ephemeral: true,
+        });
     }
 });
 
+// Login ke Discord menggunakan token
 client.login(process.env.BOT_TOKEN);
